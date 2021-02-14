8401c0ababbbf5ee1303fe7363476985964c2ad2
Jeongsoo Park <toracle@gmail.com>
2021-02-14 17:04:45 +0900

    add a backlink xmlrpc function (#19)
    ref #19

--- tests/moinrpc-xmlrpc-test.el
+++ tests/moinrpc-xmlrpc-test.el
@@ -173,4 +173,18 @@
                                 ("params" . ["TestPage"]))))))))))
 
 
+(ert-deftest moinrpc-xmlrpc-search-backlinks-should-fire-request ()
+  (with-dummy-xml-rpc-call
+   (moinrpc-xmlrpc-search-backlinks *fixture-wiki* "TestPage")
+   (should (equal *moinrpc-xmlrpc-test-call-history*
+                  '((:server-url
+                     "https://wiki.net/?action=xmlrpc2"
+                     :method
+                     system.multicall
+                     :params (((("methodName" . "applyAuthToken")
+                                ("params" . ["api-token"]))
+                               (("methodName" . "searchPages")
+                                ("params" . ["linkto:TestPage"]))))))))))
+
+
 (provide 'moinrpc-xmlrpc-test)
